8189ce3770c5ced3913115d22c293f72bd811b1e
happy436 <lelouch436@gmail.com>
2024-06-01 08:32:33 +0300

    alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import path from 'path';
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+            "@lib": path.resolve(__dirname, "./src/lib"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@utils": path.resolve(__dirname, "./src/utils"),
+		},
+	},
 });
