c0ba63b13c31aea72fd307f0a212242b65ede849
Olev Vildyakskin <lelouch436@gmail.com>
2024-06-08 19:48:01 +0300

    measurement page

--- client/src/pages/measurements.tsx
+++ client/src/pages/measurements.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Page from "../components/common/page";
+import { DatePicker } from "@tremor/react";
 
-type measurementsProps = {
-    
+type measurementsProps = {};
+
+const Measurements: React.FC<measurementsProps> = () => {
+	const [activeDay, setActiveDay] = useState(0);
+	useEffect(() => {
+		const today = Date.now();
+		const timestamp = new Date(
+			new Date(today).setHours(0, 0, 0, 0)
+		).getTime();
+		setActiveDay(timestamp);
+	}, []);
+	const handleChange = (value: number) => {
+		if (value !== undefined) {
+			const date = new Date(value);
+			const timestamp = date.getTime();
+			setActiveDay(timestamp);
+		}
+	};
+	return (
+		<Page>
+			<Page.PageTitle>Measurements</Page.PageTitle>
+			<Page.PageContent>
+				<section>
+					{activeDay !== 0 && (
+						<DatePicker
+							onValueChange={handleChange}
+							defaultValue={new Date(activeDay)}
+						/>
+					)}
+				</section>
+			</Page.PageContent>
+		</Page>
+	);
 };
-
-const Measurements:React.FC<measurementsProps> = () => {
-    
-    return <div>Measurements</div>
-}
 export default Measurements;
