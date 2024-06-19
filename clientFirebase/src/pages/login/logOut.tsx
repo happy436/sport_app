f1dd2020efe9797842ac3254ebd2a3d1d9af05b2
happy436 <lelouch436@gmail.com>
2024-06-19 15:37:09 +0300

    logout page

--- clientFirebase/src/pages/login/logOut.tsx
+++ clientFirebase/src/pages/login/logOut.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { getAuth, signOut } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../store/users";
+import { useNavigate } from "react-router-dom";
 
 type logOutProps = {};
 
-const logOut: React.FC<logOutProps> = () => {
-	
+const LogOut: React.FC<logOutProps> = () => {
+	const dispatch = useDispatch();
 	const auth = getAuth();
+    const navigation = useNavigate()
 	signOut(auth)
 		.then(() => {
-			// Sign-out successful.
+			dispatch(logOut());
+            navigation("/")
 		})
 		.catch((error) => {
 			// An error happened.
 		});
 	return <div>Loading...</div>;
 };
-export default logOut;
+export default LogOut;
