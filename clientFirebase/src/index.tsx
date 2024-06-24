a192e74850db7f4423890e3c434bf7a3a3aae5f0
happy436 <lelouch436@gmail.com>
2024-06-24 19:36:07 +0300

    types

--- clientFirebase/src/index.tsx
+++ clientFirebase/src/index.tsx
@@ -3,13 +3,12 @@ import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./app";
-import { createStore } from "./store/createStore.ts";
+import { createStore } from "./store/createStore";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const store = createStore();
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
--- clientFirebase/src/pages/measurements/measurement.tsx
+++ clientFirebase/src/pages/measurements/measurement.tsx
@@ -5,38 +5,70 @@ import {
 	AreaChart,
 	Button,
 } from "@tremor/react";
+import React, { ComponentType } from "react";
 
-type measurementProps = {
-	data: measurementData;
+type MeasurementProps = {
+	data: MeasurementData;
 	date: number;
-	setIsOpenModal: () => void;
-	setEditedCategoryId: () => void;
+	setIsOpenModal: React.Dispatch<
+		React.SetStateAction<{ addMeasure: boolean }>
+	>;
+	setEditedCategoryId: React.Dispatch<React.SetStateAction<string>>;
 };
 
-type measurementData = {
+type MeasurementData = {
 	_id: string;
 	name: string;
-	measurements: measure[];
+	measurements: Measure[];
 	units: string;
 };
 
-type measure = {
+type Measure = {
 	date: string;
 	value: number;
 };
 
-const Measurement: React.FC<measurementProps> = ({
+type CustomTooltipProps = {
+    active:boolean,
+    label:undefined,
+    payload:[]
+}
+
+type CategoryTypes = {
+    chartType:string | undefined,
+    className:string,
+    color:string,
+    dataKey:string,
+    fill:string,
+    fillOpacity:number,
+    formatter:string | undefined,
+    hide:boolean,
+    name:string,
+    payload:[],
+    stroke:string,
+    strokeLinecap:string,
+    strokeLinejoin:string,
+    strokeOpacity:number,
+    strokeWidth:number,
+    type:string | undefined,
+    unit:string,
+    value:number,
+}
+
+const Measurement: React.FC<MeasurementProps> = ({
 	data,
 	date,
 	setIsOpenModal,
 	setEditedCategoryId,
 }) => {
-	const customTooltip = (props) => {
+	const customTooltip = (props: CustomTooltipProps): JSX.Element | undefined | null => {
 		const { payload, active } = props;
 		if (!active || !payload) return null;
 		return (
 			<div className="w-56 rounded-tremor-default border border-tremor-border bg-tremor-background p-2 text-tremor-default shadow-tremor-dropdown">
-				{payload.map((category, idx) => {
+				{payload.map((category: CategoryTypes, idx: any) => {
+                    console.log(category)
+                    console.log(idx)
 					return (
 						<div key={idx} className="flex flex-1 space-x-2.5">
 							<div
@@ -57,32 +89,36 @@ const Measurement: React.FC<measurementProps> = ({
 		);
 	};
 
-    function sortObjectsByDate(arr) {
-        return [...arr].sort((a, b) => {
-            const [dayA, monthA, yearA] = a.date.split('.').map(Number);
-            const [dayB, monthB, yearB] = b.date.split('.').map(Number);
-    
-            const dateA = new Date(`20${yearA}`, monthA - 1, dayA);
-            const dateB = new Date(`20${yearB}`, monthB - 1, dayB);
-    
-            return dateA - dateB;
-        });
-    }
+	function sortObjectsByDate(arr: Measure[]): Measure[] {
+		return [...arr].sort((a, b) => {
+			const [dayA, monthA, yearA] = a.date.split(".").map(Number);
+			const [dayB, monthB, yearB] = b.date.split(".").map(Number);
 
-	const getClosestData = (data, timestamp) => {
-        const sortedData = sortObjectsByDate(data)
+			const dateA = new Date(`20${yearA}`, monthA - 1, dayA);
+			const dateB = new Date(`20${yearB}`, monthB - 1, dayB);
+
+			return dateA.getTime() - dateB.getTime();
+		});
+	}
+
+	const getClosestData = (data: Measure[], timestamp: number): Measure[] => {
+		const sortedData = sortObjectsByDate(data);
 		const targetDate = new Date(timestamp);
 
-		const parseDate = (dateString) => {
+		const parseDate = (dateString: string): Date => {
 			const [day, month, year] = dateString.split(".").map(Number);
 			return new Date(year + 2000, month - 1, day);
 		};
 
 		const closestIndex = sortedData.reduce((closest, current, index) => {
 			const currentDate = parseDate(current.date);
-			const currentDiff = Math.abs(currentDate - targetDate);
+			const currentDiff = Math.abs(
+				currentDate.getTime() - targetDate.getTime()
+			);
 			const closestDate = parseDate(sortedData[closest].date);
-			const closestDiff = Math.abs(closestDate - targetDate);
+			const closestDiff = Math.abs(
+				closestDate.getTime() - targetDate.getTime()
+			);
 			return currentDiff < closestDiff ? index : closest;
 		}, 0);
 
@@ -90,41 +126,40 @@ const Measurement: React.FC<measurementProps> = ({
 		const result = sortedData.slice(start, closestIndex + 1);
 		return result;
 	};
+
 	return (
-		<>
-			<Accordion>
-				<AccordionHeader className="text-sm font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-					{data.name}
-				</AccordionHeader>
-				<AccordionBody className="leading-6 flex flex-col items-center gap-3">
-					{data.measurements ? (
-						<AreaChart
-							className="mt-4 h-72"
-							data={getClosestData(data.measurements, date)}
-							index="date"
-							categories={["value"]}
-							colors={["blue"]}
-							yAxisWidth={30}
-							customTooltip={customTooltip}
-						/>
-					) : (
-						"Empty"
-					)}
-					<Button
-						className=""
-						onClick={() => {
-							setIsOpenModal((prev) => ({
-								...prev,
-								addMeasure: true,
-							}));
-							setEditedCategoryId(data._id);
-						}}
-					>
-						Add Measure
-					</Button>
-				</AccordionBody>
-			</Accordion>
-		</>
+		<Accordion>
+			<AccordionHeader className="text-sm font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
+				{data.name}
+			</AccordionHeader>
+			<AccordionBody className="leading-6 flex flex-col items-center gap-3">
+				{data.measurements ? (
+					<AreaChart
+						className="mt-4 h-72"
+						data={getClosestData(data.measurements, date)}
+						index="date"
+						categories={["value"]}
+						colors={["blue"]}
+						yAxisWidth={30}
+						customTooltip={customTooltip}
+					/>
+				) : (
+					"Empty"
+				)}
+				<Button
+					className=""
+					onClick={() => {
+						setIsOpenModal((prev) => ({
+							...prev,
+							addMeasure: true,
+						}));
+						setEditedCategoryId(data._id);
+					}}
+				>
+					Add Measure
+				</Button>
+			</AccordionBody>
+		</Accordion>
 	);
 };
 
